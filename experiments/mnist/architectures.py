87356c6f16b26a4d94fbc7e8eadce80431132f1c
Cassiano Becker <cassiano.becker@gmail.com>
2020-05-14 21:21:17 -0400

    simulation of diffusion weighted images for mnist and kmnist datasets (experiments2)

--- experiments/mnist/architectures.py
+++ experiments/mnist/architectures.py
@@ -62,23 +62,25 @@ class DNN2(nn.Module):
         super(DNN2, self).__init__()
 
         img_channels = img_dims[0]
-        kernel_dims1 = [3, 3, 2]
-        kernel_dims2 = [2, 2, 2]
-        strides1 = [2, 2, 2]
-        strides2 = [2, 2, 2]
-        c_out1 = 2*4*10
-        c_out2 = 3*4*10
+        kernel_dims1 = [3, 3, 3]
+        kernel_dims2 = [3, 3, 2]
+        strides1 = [1, 1, 1]
+        strides2 = [1, 1, 1]
+        c_out1 = 32
+        c_out2 = 64
         pool_size = 2
 
         # self.conv1 = DwiConv3dTorchVectChol(img_channels, c_out1, half_precision=half_precision)
-        self.conv1 = DwiConv3dTorchVect(img_channels, c_out1, kernel_dims1, strides1, half_precision=half_precision)
-        self.conv2 = nn.Conv3d(c_out1, c_out2, kernel_dims2, strides2)
-        # self.conv3 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
-        self.max1 = nn.MaxPool3d(pool_size)
+        self.conv0 = DwiConv3dTorchVect(img_channels, c_out1, kernel_dims1, strides1, half_precision=half_precision)
+        self.conv1 = nn.Conv3d(c_out1, c_out2, kernel_dims2, strides2)
+        self.conv2 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
         self.dropout1 = nn.Dropout3d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        # self.dropout3 = nn.Dropout2d(0.25)
-        linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv1, self.conv2, self.max1])
+        self.max1 = nn.MaxPool3d(pool_size)
+
+        # linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv0, self.conv1, self.conv2, self.max1])
+
+        linear_size1 = 15488
         linear_size2 = 128
 
         self.fc1 = nn.Linear(int(linear_size1), linear_size2)
@@ -86,14 +88,13 @@ class DNN2(nn.Module):
 
     def forward(self, x):
 
+        x = self.conv0(x)
         x = self.conv1(x)
         x = func.relu(x)
         x = self.conv2(x)
+        x = func.relu(x)
         x = self.max1(x)
         x = self.dropout1(x)
-        # x = func.relu(x)
-        # x = self.conv3(x)
-        # x = self.dropout3(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = func.relu(x)
