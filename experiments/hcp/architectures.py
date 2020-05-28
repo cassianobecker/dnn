bac4f64ff86cd1c6afdb56faecfc932814bcc3c5
Cassiano Becker <cassiano.becker@gmail.com>
2020-05-28 10:58:04 -0400

    hcp experiment updates for resampling

--- experiments/hcp/architectures.py
+++ experiments/hcp/architectures.py
@@ -72,10 +72,66 @@ class DnnHcp(nn.Module):
         return output
 
 
-class DnnHcpUnitKernel(nn.Module):
+class CnnHcp(nn.Module):
 
     def __init__(self, img_dims, number_of_classes, cholesky_weights=False):
-        super(DnnHcpUnitKernel, self).__init__()
+        super(CnnHcp, self).__init__()
+
+        k = 5
+        s = 2
+
+        img_channels = img_dims[0]
+        kernel_dims1 = [k, k, k]
+        kernel_dims2 = [k, k, k]
+        strides1 = [s, s, s]
+        strides2 = [s, s, s]
+        c_out1 = 128
+        c_out2 = 64
+        pool_size = 2
+
+        self.conv0 = nn.Conv3d(img_channels, c_out1, kernel_dims1, strides1)
+        self.conv1 = nn.Conv3d(c_out1, c_out2, kernel_dims1, strides1)
+        self.conv2 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
+
+        self.dropout1 = nn.Dropout3d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.max1 = nn.MaxPool3d(pool_size)
+
+        linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv0, self.conv1, self.conv2, self.max1])
+
+        #linear_size1 = 28224
+        linear_size2 = 128
+
+        self.fc1 = nn.Linear(int(linear_size1), linear_size2)
+        self.fc2 = nn.Linear(linear_size2, number_of_classes)
+
+    def forward(self, x):
+
+        x = self.conv0(x)
+
+        x = self.conv1(x)
+        x = func.relu(x)
+
+        x = self.conv2(x)
+        x = func.relu(x)
+        x = self.max1(x)
+        x = self.dropout1(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = func.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        output = func.log_softmax(x, dim=1)
+
+        return output
+
+
+class DnnHcpUnitKernel1(nn.Module):
+
+    def __init__(self, img_dims, number_of_classes, cholesky_weights=False):
+        super(DnnHcpUnitKernel1, self).__init__()
 
         k = 5
         s = 3
@@ -128,10 +184,11 @@ class DnnHcpUnitKernel(nn.Module):
         return output
 
 
-class CnnHcp(nn.Module):
+# larger pool size, smaller stride
+class DnnHcpUnitKernel2(nn.Module):
 
     def __init__(self, img_dims, number_of_classes, cholesky_weights=False):
-        super(CnnHcp, self).__init__()
+        super(DnnHcpUnitKernel2, self).__init__()
 
         k = 5
         s = 2
@@ -143,9 +200,9 @@ class CnnHcp(nn.Module):
         strides2 = [s, s, s]
         c_out1 = 128
         c_out2 = 64
-        pool_size = 2
+        pool_size = 4
 
-        self.conv0 = nn.Conv3d(img_channels, c_out1, kernel_dims1, strides1)
+        self.conv0 = DwiConv3dUnitKernel(img_channels, c_out1, cholesky_weights=cholesky_weights)
         self.conv1 = nn.Conv3d(c_out1, c_out2, kernel_dims1, strides1)
         self.conv2 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
 
@@ -155,7 +212,7 @@ class CnnHcp(nn.Module):
 
         linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv0, self.conv1, self.conv2, self.max1])
 
-        #linear_size1 = 28224
+        # linear_size1 = 28224
         linear_size2 = 128
 
         self.fc1 = nn.Linear(int(linear_size1), linear_size2)
@@ -170,6 +227,177 @@ class CnnHcp(nn.Module):
 
         x = self.conv2(x)
         x = func.relu(x)
+        x = self.max1(x)
+        x = self.dropout1(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = func.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        output = func.log_softmax(x, dim=1)
+
+        return output
+
+
+# larger kernel, larger pool size
+class DnnHcpUnitKernel3(nn.Module):
+
+    def __init__(self, img_dims, number_of_classes, cholesky_weights=False):
+        super(DnnHcpUnitKernel3, self).__init__()
+
+        k = 7
+        s = 3
+
+        img_channels = img_dims[0]
+        kernel_dims1 = [k, k, k]
+        kernel_dims2 = [k, k, k]
+        strides1 = [s, s, s]
+        strides2 = [s, s, s]
+        c_out1 = 128
+        c_out2 = 64
+        pool_size = 5
+
+        self.conv0 = DwiConv3dUnitKernel(img_channels, c_out1, cholesky_weights=cholesky_weights)
+        self.conv1 = nn.Conv3d(c_out1, c_out2, kernel_dims1, strides1)
+        self.conv2 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
+
+        self.dropout1 = nn.Dropout3d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.max1 = nn.MaxPool3d(pool_size)
+
+        linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv0, self.conv1, self.conv2, self.max1])
+
+        # linear_size1 = 28224
+        linear_size2 = 128
+
+        self.fc1 = nn.Linear(int(linear_size1), linear_size2)
+        self.fc2 = nn.Linear(linear_size2, number_of_classes)
+
+    def forward(self, x):
+
+        x = self.conv0(x)
+
+        x = self.conv1(x)
+        x = func.relu(x)
+
+        x = self.conv2(x)
+        x = func.relu(x)
+        x = self.max1(x)
+        x = self.dropout1(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = func.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        output = func.log_softmax(x, dim=1)
+
+        return output
+
+
+# less dropout
+class DnnHcpUnitKernel4(nn.Module):
+
+    def __init__(self, img_dims, number_of_classes, cholesky_weights=False):
+        super(DnnHcpUnitKernel4, self).__init__()
+
+        k = 5
+        s = 3
+
+        img_channels = img_dims[0]
+        kernel_dims1 = [k, k, k]
+        kernel_dims2 = [k, k, k]
+        strides1 = [s, s, s]
+        strides2 = [s, s, s]
+        c_out1 = 128
+        c_out2 = 64
+        pool_size = 2
+
+        self.conv0 = DwiConv3dUnitKernel(img_channels, c_out1, cholesky_weights=cholesky_weights)
+        self.conv1 = nn.Conv3d(c_out1, c_out2, kernel_dims1, strides1)
+        self.conv2 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
+
+        self.dropout1 = nn.Dropout3d(0.2)
+        self.dropout2 = nn.Dropout2d(0.2)
+        self.max1 = nn.MaxPool3d(pool_size)
+
+        linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv0, self.conv1, self.conv2, self.max1])
+
+        # linear_size1 = 28224
+        linear_size2 = 128
+
+        self.fc1 = nn.Linear(int(linear_size1), linear_size2)
+        self.fc2 = nn.Linear(linear_size2, number_of_classes)
+
+    def forward(self, x):
+
+        x = self.conv0(x)
+
+        x = self.conv1(x)
+        x = func.relu(x)
+
+        x = self.conv2(x)
+        x = func.relu(x)
+        x = self.max1(x)
+        x = self.dropout1(x)
+
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = func.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        output = func.log_softmax(x, dim=1)
+
+        return output
+
+
+# less dropout
+class DnnHcpUnitKernelShallow(nn.Module):
+
+    def __init__(self, img_dims, number_of_classes, cholesky_weights=False):
+        super(DnnHcpUnitKernelShallow, self).__init__()
+
+        k = 5
+        s = 3
+
+        img_channels = img_dims[0]
+        kernel_dims1 = [k, k, k]
+        kernel_dims2 = [k, k, k]
+        strides1 = [s, s, s]
+        strides2 = [s, s, s]
+        c_out1 = 128
+        c_out2 = 64
+        pool_size = 2
+
+        self.conv0 = DwiConv3dUnitKernel(img_channels, c_out1, cholesky_weights=cholesky_weights)
+        self.conv1 = nn.Conv3d(c_out1, c_out2, kernel_dims1, strides1)
+        # self.conv2 = nn.Conv3d(c_out2, c_out2, kernel_dims2, strides2)
+
+        self.dropout1 = nn.Dropout3d(0.2)
+        self.dropout2 = nn.Dropout2d(0.2)
+        self.max1 = nn.MaxPool3d(pool_size)
+
+        linear_size1 = Dimensions().dimensions_for_linear(img_dims, [self.conv0, self.conv1, self.conv2, self.max1])
+
+        # linear_size1 = 28224
+        linear_size2 = 128
+
+        self.fc1 = nn.Linear(int(linear_size1), linear_size2)
+        self.fc2 = nn.Linear(linear_size2, number_of_classes)
+
+    def forward(self, x):
+
+        x = self.conv0(x)
+
+        x = self.conv1(x)
+        x = func.relu(x)
+
+        # x = self.conv2(x)
+        # x = func.relu(x)
         x = self.max1(x)
         x = self.dropout1(x)
 
