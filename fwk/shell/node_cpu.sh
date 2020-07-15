bc222b229b7ee6ca4729a39f8ed6641f3307eca5
Cassiano Becker <cassiano.becker@gmail.com>
2020-07-15 16:25:03 -0400

    updates for synthetic dataset generation with fiberfox

--- fwk/shell/node_cpu.sh
+++ fwk/shell/node_cpu.sh
@@ -2,8 +2,10 @@
 
 source activate dnn-cluster
 
-module load mrtrix
+#module load mrtrix
 
 echo Host name: `hostname`
+
+cd ~/dnn
 
 python -u -c "from fwk.experiment import Experiment; Experiment.run('${1}')"
